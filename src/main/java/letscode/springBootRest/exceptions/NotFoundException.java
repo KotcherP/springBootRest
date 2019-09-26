0a01808ca89c485b07eacb583d906bdf103d65a0
KotcherP <chernobai88@gmail.com>
2019-09-26 09:50:10 +0300

    Initial commit

---
+++ src/main/java/letscode/springBootRest/exceptions/NotFoundException.java
@@ -0,0 +1,8 @@
+package letscode.springBootRest.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class NotFoundException extends RuntimeException {
+}
